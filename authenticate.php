09fe7d609b33d29ec19cedeb4b547a755f456d4c
Hans Andersson <hans@sif.stanford.edu>
2014-03-08 08:14:04 -0800

    (Sorry… I’m still getting in the habit of writing better comments. I’ve added comments to the scripts I just pushed to the repository.)

--- authenticate.php
+++ authenticate.php
@@ -12,6 +12,7 @@ if (!!session_id() && strlen(session_id()) > 0)
 
 if (isset ($_POST["handle"]) && isset ($_POST["password"]))
 {
+	//  Validate the posted data
 	$handle = strtolower(urldecode($_POST["handle"]));
 	$password = urldecode($_POST["password"]);
 	
@@ -25,6 +26,7 @@ if (isset ($_POST["handle"]) && isset ($_POST["password"]))
 		exit_with_error("Invalid Handle", "Handle must consist of between 4 and 63 (inclusive) alphanumeric characters beginning with a letter.");
 	}
 	
+	//  See whether we can authenticate with the handle and password posted
 	$result = $mysqli->query(sprintf("SELECT user_id AS id, handle, email, name_given AS nameGiven, name_family AS nameFamily FROM users WHERE handle = '%s' AND pswd_hash = '%s'",
 		$mysqli->escape_string($handle),
 		$mysqli->escape_string(crypt($password, $password))
@@ -39,18 +41,19 @@ if (isset ($_POST["handle"]) && isset ($_POST["password"]))
 		
 		$user_assoc["id"] = intval($user_assoc["id"]);
 		
+		//  Start a PHP session (using cookies), and update the database accordingly
 		$mysqli->query(sprintf("UPDATE users SET session = '%s' WHERE user_id = %d",
 			$mysqli->escape_string($session),
 			$user_assoc["id"]
 		));
 		
+		//  Tell the front end that we succeeded in authenticating
 		exit_with_result($user_assoc);
 	}
 	else
 	{
 		exit_with_error("Invalid Credentials", "The handle and password entered match no users in the database.");
 	}
-	exit;
 }
 
 exit_with_error("Invalid Post", "Authentication post must include handle and password.");
--- register.php
+++ register.php
@@ -12,6 +12,7 @@ if (!!session_id() && strlen(session_id()) > 0)
 
 if (isset ($_POST["email"]) && isset ($_POST["handle"]) && isset ($_POST["password"]))
 {
+	//  Validate the posted data
 	$email = strtolower(urldecode($_POST["email"]));
 	$handle = strtolower(urldecode($_POST["handle"]));
 	$password = urldecode($_POST["password"]);
@@ -31,6 +32,7 @@ if (isset ($_POST["email"]) && isset ($_POST["handle"]) && isset ($_POST["passwo
 		exit_with_error("Invalid Handle", "Handle must consist of between 4 and 63 (inclusive) alphanumeric characters beginning with a letter.");
 	}
 	
+	//  Check whether requested handle conflicts with any existing handle
 	$result = $mysqli->query(sprintf("SELECT * FROM users WHERE handle = '%s'",
 		$mysqli->escape_string($handle)
 	));
@@ -41,6 +43,8 @@ if (isset ($_POST["email"]) && isset ($_POST["handle"]) && isset ($_POST["passwo
 	}
 	$result->close();
 	
+	
+	//  Good to go, so insert the new user
 	$mysqli->query(sprintf("INSERT INTO users (handle, email, pswd_hash) VALUES ('%s', '%s', '%s')",
 		$mysqli->escape_string($handle),
 		$mysqli->escape_string($email),
@@ -51,6 +55,7 @@ if (isset ($_POST["email"]) && isset ($_POST["handle"]) && isset ($_POST["passwo
 		$mysqli->escape_string($handle)
 	));
 	
+	//  Just make sure we actually created the user
 	if (!$result)
 	{
 		exit_with_error("Unknown Error", "The back end unexpectedly failed to create the user.");
@@ -58,6 +63,7 @@ if (isset ($_POST["email"]) && isset ($_POST["handle"]) && isset ($_POST["passwo
 	$user_assoc = $result->fetch_assoc();
 	$result->close();
 	
+	//  Finally, send the user information to the front end
 	exit_with_result($user_assoc);
 }
 
