bb3299466eca9e71379fa63e37e1da5a5c4062a7
Hans Andersson <hans@dn200ol8c.stanford.edu>
2014-03-12 16:28:38 -0700

    Added “classes.php”, which calls “require_once” for all class-definition scripts.

---
+++ backend/classes.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset ($classes)) exit;
+$classes = true;
+
+require_once "classes/session.php";
+/*
+require_once "classes/entry.php";
+require_once "classes/course.php";
+require_once "classes/user.php";
+require_once "classes/list.php";
+require_once "classes/test.php";
+*/
+
+?>
