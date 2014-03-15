10ec7eab81fc0c62246e10d7437eb6e7e49c7c8a
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 08:57:19 -0700

    Drafted; still some to-do.

--- backend/classes/list.php
+++ backend/classes/list.php
@@ -11,12 +11,12 @@ class EntryList
 	private $share_public;
 	private $user_id;
 	
-	private function __construct($list_id, $share_public, $title = null, $user_id = null)
+	private function __construct($list_id, $share_public, $list_name = null, $user_id = null)
 	{
 		$this->list_id = intval($list_id, 10);
 		$this->list_name = $list_name;
 		$this->shared_public = $share_public;
-		$this->title = title
+		$this->list_name = list_name
 		$this->user_id = $user_id;
 	}
 	
@@ -42,6 +42,20 @@ class EntryList
 		return !!Session::$user && (Session::$user->user_id === $this->user_id);
 	}
 	
+	public function delete()
+	{
+		global $mysqli;
+		
+		if (!Session::$user) return null;
+		
+		$mysqli->query(sprintf("DELETE FROM lists WHERE user_id = %d AND list_id = %d",
+			Session::$user->user_id,
+			$this->list_id
+		));
+		
+		return self::select($mysqli->insert_id);
+	}
+	
 	public function get_entries()
 	{
 		global $mysqli;
@@ -63,6 +77,20 @@ class EntryList
 			}
 		}
 		return $this->entries;
+	}
+	
+	public static function insert($list_name = null)
+	{
+		global $mysqli;
+		
+		if (!Session::$user) return null;
+		
+		$mysqli->query(sprintf("INSERT INTO lists (user_id, list_name) VALUES (%d, '%s')",
+			Session::$user->user_id,
+			$mysqli->escape_string($list_name)
+		));
+		
+		return self::select($mysqli->insert_id);
 	}
 	
 	public static function select($list_id)
@@ -97,15 +125,46 @@ class EntryList
 	
 	//  Adds an entry to this list
 	//      Returns this list
-	public function add_entry($entry)
+	public function add_entry($entry_to_add)
 	{
 		if (!session_user_can_write()) return null;
 		
 		//  Make sure $this->get_entries() doesn't already contain an entry whose entry_id === $entry->entry_id
-		//  Insert into entries from the dictionary, if necessary
+		foreach ($this->get_entries() as $entry)
+		{
+			if ($entry->entry_id === $entry_to_add->entry_id) return null;
+		}
+		
+		//  Insert into user_entries from dictionary, if necessary
+		$result = $mysqli->query(sprintf("SELECT * FROM user_entries WHERE user_id = %d AND entry_id = %d",
+			Session::$user->user_id,
+			$entry_to_add->entry_id
+		));
+		
+		if (!$result || !($result_assoc = $result->fetch_assoc()))
+		{
+			$mysqli->query(sprintf("INSERT INTO user_entries (user_id, entry_id) VALUES (%d, %d)",
+				Session::$user->user_id,
+				$entry_to_add->entry_id
+			));
+
+			$result = $mysqli->query(sprintf("SELECT * FROM user_entries WHERE user_id = %d AND entry_id = %d",
+				Session::$user->user_id,
+				$entry_to_add->entry_id
+			));
+			
+			$result_assoc = $result->fetch_assoc();
+		}
+		
+		$entry_added = Entry::from_mysql_result_assoc($result_assoc);
+		
 		//  Insert into list_entries for $this->list_id and $entry->entry_id
-		//  If everything succeeded, then return $this;
-		//      otherwise, return null
+		$mysqli->query(sprintf("INSERT INTO list_entries (list_id, entry_id) VALUES (%d, %d)",
+			$this->list_id,
+			$entry_added->entry_id
+		));
+		
+		return $this;
 	}
 	
 	//  Adds an entry to this list
@@ -114,12 +173,18 @@ class EntryList
 	{
 		if (!session_user_can_write()) return null;
 		
-		foreach ($this->get_entries() as $entry)
+		foreach ($this->get_entries() as $entry_removed)
 		{
-			if ($entry->entry_id === $entry_to_remove->entry_id)
+			if ($entry_removed->entry_id === $entry_to_remove->entry_id)
 			{
-				//  Delete from database
-				//  If deletion succeeded, then delete from $this->entries and return $this
+				$mysqli->query(sprintf("DELETE FROM list_entries (list_id, entry_id) VALUES (%d, %d)",
+					$this->list_id,
+					$entry_removed->entry_id
+				));
+				
+				$this->entries = array_diff($this->entries, array ($entry_removed));
+				
+				return $this;
 			}
 		}
 		
@@ -132,6 +197,24 @@ class EntryList
 	{
 		if (!Session::$user || !session_user_can_read()) return null;
 		//  Create a copy of the list with Session::$user as the owner
+		
+		return null;
+	}
+	
+	public function assoc_for_json()
+	{
+		$entries_returnable = array ();
+		foreach ($this->get_entries() as $entry)
+		{
+			array_push($entries_returnable, $entry->assoc_for_json());
+		}
+		
+		return array (
+			"listId" => $this->list_id,
+			"privileges" => null,
+			"listName" => $this->list_name,
+			"entries" => $entries_returnable
+		);
 	}
 }
 
