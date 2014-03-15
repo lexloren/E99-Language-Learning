78daca2e995e987d5dd2442d7e51524cb5116db7
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 06:39:19 -0700

    Drafted the script.

--- backend/classes/entry.php
+++ backend/classes/entry.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once "../connect.php";
-require_once "../support.php";
+require_once "./backend/connect.php";
+require_once "./backend/support.php";
 
 class Entry
 {
@@ -11,17 +11,31 @@ class Entry
 	public $user_id = null;
 	
 	public function __construct($entry_id, $lang_code_0, $lang_code_1,
-		$lang_0, $lang_1, $pronunciation, $user_id = null)
+		$word_0, $word_1, $pronunciation, $user_id = null)
 	{
 		$this->entry_id = intval($entry_id, 10);
 		$this->words = array (
-			$lang_code_0 => $lang_0,
-			$lang_code_1 => $lang_1
+			$lang_code_0 => $word_0,
+			$lang_code_1 => $word_1
 		);
 		$this->pronunciations = array (
 			$lang_code_1 => $pronunciation
 		);
 		$this->user_id = $user_id;
+	}
+	
+	public static function from_mysql_result_assoc($result)
+	{
+		if (!$result) return null;
+		
+		return new Entry($result["entry_id"],
+			$result["lang_code_0"],
+			$result["lang_code_1"],
+			$result["word_0"],
+			$result["word_1"],
+			$result["word_1_pronun"],
+			$result["user_id"]
+		);
 	}
 	
 	public function assoc_for_json()
