a89c7e6c0af6eab8c8844f66a9cac7134fe7f102
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 08:06:16 -0700

    Adding user-by-user privileges.

--- backend/classes/list.php
+++ backend/classes/list.php
@@ -8,17 +8,44 @@ class EntryList
 	private $list_id;
 	private $list_name;
 	private $entries;
+	private $shared_public;
 	private $user_id;
 	
-	private function __construct($list_id, $title = null, $user_id = null)
+	private function __construct($list_id, $shared_public = false, $title = null, $user_id = null)
 	{
-		$this->list_id = $list_id;
-		
+		$this->list_id = intval($list_id, 10);
+		$this->list_name = $list_name;
+		$this->shared_public = $shared_public;
+		$this->title = title
+		$this->user_id = $user_id;
+	}
+	
+	//  Returns true iff Session::$user can read this list for any reason
+	private function session_user_can_read()
+	{
+		return session_user_can_write()
+			|| session_user_can_read_via_course_sharing()
+			|| !!$this->shared_public;
+	}
+	
+	//  Returns true iff Session::$user is in any course in which this list is shared
+	private function session_user_can_read_via_course_sharing()
+	{
+		//  Stub...
+		return false;
+	}
+	
+	//  Returns true iff Session::$user owns this list
+	private function session_user_can_write()
+	{
+		return !!Session::$user && (Session::$user->user_id === $this->user_id);
 	}
 	
 	public function get_entries()
 	{
 		global $mysqli;
+		
+		//  Need to add privileges here, based on public sharing and course-wide sharing
 		
 		if (!isset ($this->entries))
 		{
@@ -41,13 +68,17 @@ class EntryList
 	{
 		global $mysqli;
 		
+		//  Need to add privileges here, based on public sharing and course-wide sharing
+		
 		$result = $mysqli->query(sprintf("SELECT * FROM lists WHERE list_id = %d",
 			intval($list_id)
 		));
 		
 		if (!!$result && !!($result_assoc = $result->fetch_assoc()))
 		{
-			return EntryList::from_mysql_result_assoc($result_assoc);
+			//  Return the list iff Session::$user can read it
+			$list = EntryList::from_mysql_result_assoc($result_assoc);
+			return $list->session_user_can_read() ? $list : null;
 		}
 		
 		return null;
@@ -66,6 +97,8 @@ class EntryList
 	//      Returns this list
 	public function add_entry($entry)
 	{
+		if (!session_user_can_write()) return null;
+		
 		//  Make sure $this->get_entries() doesn't already contain an entry whose entry_id === $entry->entry_id
 		//  Insert into entries from the dictionary, if necessary
 		//  Insert into list_entries for $this->list_id and $entry->entry_id
@@ -77,6 +110,8 @@ class EntryList
 	//      Returns this list
 	public function remove_entry($entry_to_remove)
 	{
+		if (!session_user_can_write()) return null;
+		
 		foreach ($this->get_entries() as $entry)
 		{
 			if ($entry->entry_id === $entry_to_remove->entry_id)
@@ -91,9 +126,10 @@ class EntryList
 	
 	//  Copies this list, setting the copy's owner to some other user
 	//      Returns the copy
-	public function copy_for_user($user)
+	public function copy_for_session_user()
 	{
-		
+		if (!Session::$user || !session_user_can_read()) return null;
+		//  Create a copy of the list with Session::$user as the owner
 	}
 }
 
