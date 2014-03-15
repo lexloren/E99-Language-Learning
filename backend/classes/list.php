8688c6438efa059e5e9ae8a6b9bc8ecab46ee2f0
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 05:43:01 -0700

    Drafted stub methods.

--- backend/classes/list.php
+++ backend/classes/list.php
@@ -5,7 +5,26 @@ require_once "../support.php";
 
 class List
 {
-
+	//  Adds an entry to this list
+	//      Returns this list
+	public function add_entry($entry)
+	{
+		
+	}
+	
+	//  Adds an entry to this list
+	//      Returns this list
+	public function remove_entry($entry)
+	{
+	
+	}
+	
+	//  Copies this list, setting the copy's owner to some other user
+	//      Returns the copy
+	public function copy_for_user($user)
+	{
+		
+	}
 }
 
 ?>
