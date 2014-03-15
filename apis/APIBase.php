80fd818e14ef22b114db21a457ad900f7f58a7f0
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 15:37:42 -0700

    Responded to question in comments.

--- apis/APIBase.php
+++ apis/APIBase.php
@@ -1,5 +1,9 @@
 <?php
 
+//  The purpose of this statement is to enforce that we don't try to include/require more than once (by accident).
+//  If we're getting an error, it means somehow we're trying to require this script more than once,
+//      and that means we have an error elsewhere in the application.
+
 //Do we need this for a class? Getting some error, so commented
 //if (isset ($apibase)) exit;
 //$apibase = true;
