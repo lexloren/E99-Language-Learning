5e3af8bae717b154f6ee4676564043498d8307f2
Hans Andersson <hans@sif.stanford.edu>
2014-03-15 07:48:45 -0700

    Described the functionality in the stub methods.

--- backend/classes/list.php
+++ backend/classes/list.php
@@ -6,7 +6,7 @@ require_once "./backend/support.php";
 class EntryList
 {
 	private $list_id;
-	private $title;
+	private $list_name;
 	private $entries;
 	private $user_id;
 	
@@ -14,6 +14,27 @@ class EntryList
 	{
 		$this->list_id = $list_id;
 		
+	}
+	
+	public function get_entries()
+	{
+		global $mysqli;
+		
+		if (!isset ($this->entries))
+		{
+			$result = $mysqli->query(sprintf("SELECT * FROM list_entries LEFT JOIN dictionary ON entry_id WHERE list_id = %d",
+				intval($this->list_id)
+			));
+			
+			if (!result) return null;
+			
+			$this->entries = array();
+			while (($entry_assoc = $result->fetch_assoc()))
+			{
+				array_push($this->entries, Dictionary::select_entry($entry_assoc["entry_id"]));
+			}
+		}
+		return $this->entries;
 	}
 	
 	public static function select($list_id)
@@ -24,7 +45,7 @@ class EntryList
 			intval($list_id)
 		));
 		
-		if (!!($result_assoc = $result->fetch_assoc()))
+		if (!!$result && !!($result_assoc = $result->fetch_assoc()))
 		{
 			return EntryList::from_mysql_result_assoc($result_assoc);
 		}
@@ -32,18 +53,40 @@ class EntryList
 		return null;
 	}
 	
-	//  Adds an entry to this list
-	//      Returns this list
-	public function add_entry($entry)
+	public static function from_mysql_result_assoc($result_assoc)
 	{
-		
+		return new EntryList(
+			$result_assoc["list_id"],
+			!!$result_assoc["list_name"] && strlen($result_assoc["list_name"]) > 0 ? $result_assoc["list_name"] : null,
+			$result_assoc["user_id"]
+		);
 	}
 	
 	//  Adds an entry to this list
 	//      Returns this list
-	public function remove_entry($entry)
+	public function add_entry($entry)
 	{
+		//  Make sure $this->get_entries() doesn't already contain an entry whose entry_id === $entry->entry_id
+		//  Insert into entries from the dictionary, if necessary
+		//  Insert into list_entries for $this->list_id and $entry->entry_id
+		//  If everything succeeded, then return $this;
+		//      otherwise, return null
+	}
 	
+	//  Adds an entry to this list
+	//      Returns this list
+	public function remove_entry($entry_to_remove)
+	{
+		foreach ($this->get_entries() as $entry)
+		{
+			if ($entry->entry_id === $entry_to_remove->entry_id)
+			{
+				//  Delete from database
+				//  If deletion succeeded, then delete from $this->entries and return $this
+			}
+		}
+		
+		return null;
 	}
 	
 	//  Copies this list, setting the copy's owner to some other user
