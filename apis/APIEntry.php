47fc662fd01136a1b33d11761c472f5e56885ebd
Hans Andersson <hans@dn200olga.stanford.edu>
2014-03-19 07:33:10 -0700

    Added stubs.

--- apis/APIEntry.php
+++ apis/APIEntry.php
@@ -5,15 +5,26 @@ require_once "./backend/classes.php";
 
 class APIEntry extends APIBase
 {
-	public function __construct($user, $mysqli) {	
+	public function __construct($user, $mysqli)
+	{
 		parent::__construct($user, $mysqli);
 	}
 	
+	public function update()
+	{
+		// word_0
+		// word_1
+		// word_1_pronun
+	}
 	
+	public function add_annotation()
+	{
+	
+	}
+	
+	public function remove_annotation()
+	{
+	
+	}
 }
 ?>
-
-
-
-
-
