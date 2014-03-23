335ce618bb17a269cc9c85c372fbfdec3a6ab4c7
Hans Andersson <hans@Sif.local>
2014-03-23 13:50:18 -0700

    Implemented annotation-related functionalities.

--- apis/APIEntry.php
+++ apis/APIEntry.php
@@ -17,19 +17,37 @@ class APIEntry extends APIBase
 		// word_1_pronun
 	}
 	
+	private function validate_entry_id($entry_id)
+	{
+		$entry = null;
+		if (!isset($entry_id))
+		{
+			Session::get()->set_error_assoc("Invalid Request", "Request must include entry_id.");
+		}
+		else if (!($entry = Entry::select(($entry_id = intval($entry_id, 10)))))
+		{
+			Session::get()->set_error_assoc("Unknown Entry", "Back end failed to select entry with entry_id = $entry_id.");
+		}
+		
+		return $entry;
+	}
+	
 	public function annotations()
 	{
-	
-	}
-	
-	public function annotations_add()
-	{
+		if (!Session::get()->reauthenticate()) return;
 		
-	}
-	
-	public function annotations_remove()
-	{
-		
+		if (($entry = $this->validate_entry_id($_GET["entry_id"])))
+		{
+			$entry = $entry->copy_for_session_user();
+			
+			$annotations = $entry->get_annotations();
+			foreach ($annotations as &$annotation)
+			{
+				$annotation = $annotation->assoc_for_json();
+			}
+			
+			Session::get()->set_result_assoc($annotations);
+		}
 	}
 }
 ?>
