a59441f77f10d69fbfce72a4fcb3fdf17d4dc367
Hans Andersson <hans@dn200ol0b.stanford.edu>
2014-03-26 14:36:10 -0700

    Implemented the directory of users.

---
+++ backend/classes/directory.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "./backend/connection.php";
+require_once "./backend/classes.php";
+
+class Directory
+{
+	public static function look_up($query)
+	{
+		$mysqli = Connection::get_shared_instance();
+		
+		$query = $mysqli->escape_string($query);
+		
+		$result = $mysqli->query("SELECT * FROM users WHERE email LIKE '$query' OR handle LIKE '$query'");
+		
+		$users = array ();
+		while (($result_assoc = $result->fetch_assoc()))
+		{
+			array_push($users, User::from_mysql_result_assoc($result_assoc));
+		}
+		return $users;
+	}
+}
+
+?>
