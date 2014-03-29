9991196b7ddb700e18f513759726dca174be0fae
Arunabha nag <nag.arunabha@gmail.com>
2014-03-29 18:46:59 -0400

    Added dictionary_query.php

--- apis/APIDictionary.php
+++ apis/APIDictionary.php
@@ -12,7 +12,30 @@ class APIDictionary extends APIBase
 	
 	public function query()
 	{
-	
+		if (self::validate_request($_GET, array ("word", "langs")))
+		{
+			$langs = $_GET["langs"];		
+			$pagination = null;
+			if (isset($_GET["page_size"]) && isset($_GET["page_num"]))
+			{
+				$pagination = Array(
+				'size' => $_GET["page_size"],
+				'num' => $_GET["page_num"]
+				);
+			}
+			
+			$entries = Dictionary::look_up($_GET["word"], $langs, $pagination);
+
+			if (isset($entries))
+			{
+				$entries_returnable = array ();
+				foreach ($entries as $entry)
+				{
+					array_push($entries_returnable, $entry->assoc_for_json());
+				}
+				Session::get()->set_result_assoc($entries_returnable);
+			}
+		}
 	}
 }
 ?>
