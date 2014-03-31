145ac31b7162cf513addbe09b5f79a80a718631c
nirmalv <nirmalv@localhost>
2014-03-31 12:29:36 -0700

    Fix for issue #19 (errors for empty lists)

--- backend/classes/practice.php
+++ backend/classes/practice.php
@@ -34,17 +34,23 @@ class Practice
 		$not_learned_entry_set = self::from_mysql_entry_id_assoc($not_learned_entries)->get_entry_ids();
 		$not_learned_count = count($not_learned_entry_set);
 		
-		$new_entry_count = min(
-			$not_learned_count,
-			ceil($not_learned_count / ($learned_count + $not_learned_count) * $count_limit)
-		);
+		if (($learned_count + $not_learned_count) == 0 || $count_limit == 0)
+		{
+			return Practice(array());
+		}
+		else
+		{
+			$new_entry_count = min(
+				$not_learned_count,
+				ceil($not_learned_count / ($learned_count + $not_learned_count) * $count_limit)
+			);
 		
-		$practice_entry_set = array_merge(
-			array_slice($not_learned_entry_set, 0, $new_entry_count),
-			array_slice($learned_entry_set, 0, $count_limit - $new_entry_count)
-		);
-		
-		return new Practice($practice_entry_set);
+			$practice_entry_set = array_merge(
+				array_slice($not_learned_entry_set, 0, $new_entry_count),
+				array_slice($learned_entry_set, 0, $count_limit - $new_entry_count)
+			);
+			return new Practice($practice_entry_set);
+		}
 	}
 	
 	private $entry_ids = null;
