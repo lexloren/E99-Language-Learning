6441562b4964da0c68317c82053be58bd2240977
unknown <lloren@gmail.com>
2014-04-02 07:32:02 -0400

    Updated API handling.

--- frontend/src/js/practice.js
+++ frontend/src/js/practice.js
@@ -3,16 +3,19 @@
  * 
  */
 
-var URL = "/";
+var URL = "http://cscie99.fictio.us/";
 var showWord = true;
 var showPronun = false;
 var showTrans = false;
 var wordList = [];
 
 /* mockjax for testing */
+var listsURL = URL + 'user_lists.php';
+var practiceURL = URL + 'user_practice.php';
+var dictionaryURL = URL + 'query_dictionary.php';
 
 $.mockjax({
-  url: 'user_lists.php',
+  url: listsURL,
   responseText: {
 	"isError":false,
 	"errorTitle":null,
@@ -26,38 +29,70 @@ $.mockjax({
 });
 
 $.mockjax({
-  url: 'user_practice.php',
+  url: practiceURL,
   responseText: {
-	"isError":false,
+	"isError":false, 
 	"errorTitle":null,
 	"errorDescription":null,
-	"result":[{
-		"entryId":"1",
-		"privileges":"",
-		"word":"Word 1",
-		"translation":"Translation 1",
-		"pronunciation":"Pronunciation 1",
-		"userId":null,
-		"annotations":[]},
-		{"entryId":"2",
-		"privileges":"",
-		"word":"Word 2",
-		"translation":"Translation 2",
-		"pronunciation":"Pronunciation 2",
-		"userId":null,
-		"annotations":[]},
-		{"entryId":"3",
-		"privileges":"",
-		"word":"Word 3",
-		"translation":"Translation 3",
-		"pronunciation":"Pronunciation 3",
-		"userId":null,
-		"annotations":[]},],
-	"resultInformation":{"entriesCount":2,"pageSize":2,"pageNum":1}} 
+	"result":[
+	{
+		"entryId":12003,
+		"languages":["en","jp"],
+		"owner":{
+			"userId":6,
+			"isSessionUser":true,
+			"handle":"practitioner",
+			"email":"lloren@gmail.com",
+			"nameGiven":"","nameFamily":""
+		},
+		"words":{
+			"en":"(n) toughness (of a material)",
+			"jp":"\u3058\u3093\u6027"
+		},
+		"pronuncations":{
+			"jp":"\u3058\u3093\u305b\u3044"
+		}
+	},
+	{
+		"entryId":28,
+		"languages":["en","jp"],
+		"owner":{
+			"userId":6,
+			"isSessionUser":true,
+			"handle":"practitioner",
+			"email":"lloren@gmail.com",
+			"nameGiven":"","nameFamily":""
+		},
+		"words":{
+			"en":"fastening",
+			"jp":"\u3006"
+		},
+		"pronuncations":{
+			"jp":"\u3057\u3081"
+		}
+	},
+	{
+		"entryId":50234,
+		"languages":["en","jp"],
+		"owner":{
+			"userId":6,
+			"isSessionUser":true,
+			"handle":"practitioner",
+			"email":"lloren@gmail.com",
+			"nameGiven":"","nameFamily":""
+		},
+		"words":{
+			"en":"(n) (comp) Gopher",
+			"jp":"\u30b4\u30fc\u30d5\u30a1\u30fc"
+		},
+		"pronuncations":{
+			"jp":null
+		}
+	}],"resultInformation":null}
 });
 
 $.mockjax({
-  url: 'query_dictionary.php',
+  url: dictionaryURL,
   responseText: {
 	"isError":false,
 	"errorTitle":null,
@@ -99,6 +134,36 @@ function setupDoc() {
 	$('#card-followup-container').hide();
 }
 
+function populate_word() {
+	$('#flashcard-word-panel').html(getWord(wordList[0].words[wordList[0].languages[1]]));	
+}
+
+function populate_pronoun() {
+	$('#flashcard-pronounce-panel').html(wordList[0].pronuncations[wordList[0].languages[1]]);
+}
+
+function populate_trans() {
+	$('#flashcard-trans-panel').html(wordList[0].words[wordList[0].languages[0]]);
+}
+
+function show_full_hide_empty() {
+	if (wordList[0].words[wordList[0].languages[1]] === null) {
+		$('#flashcard-word').hide();
+	} else {
+		$('#flashcard-word').show();
+	}
+	if (wordList[0].pronuncations[wordList[0].languages[1]] === null) {
+		$('#flashcard-pronounce').hide();
+	} else {
+		$('#flashcard-pronounce').show();
+	}
+	if (wordList[0].words[wordList[0].words[wordList[0].languages[0]]] === null) {
+		$('#flashcard-trans').hide();
+	} else {
+		$('#flashcard-trans').show();
+	}
+}
+
 function handleClicks() {
 
 	/* send selected lists to server, get cards to practice with */
@@ -124,19 +189,19 @@ function handleClicks() {
 	$(document).on('click', '.dictionary-close', function () {
 		$('#translation-panel').hide();
 	});	
-
+	
 	/* show hidden cards */
 	$('#flashcard-word-button').click(function(event) {
 		event.preventDefault();
-		$('#flashcard-word-panel').html(getWord(wordList[0].word));
+		populate_word();
 	});
 	$('#flashcard-pronounce-button').click(function(event) {
 		event.preventDefault();
-		$('#flashcard-pronounce-panel').html(wordList[0].pronunciation);
+		populate_pronoun();
 	});
 	$('#flashcard-trans-button').click(function(event) {
 		event.preventDefault();
-		$('#flashcard-trans-panel').html(wordList[0].translation);
+		populate_trans();
 	});
 
 	/* open menu to select a new deck */
@@ -157,15 +222,18 @@ function getWord(word) {
 }
 
 function shiftCards() {
-	var temp = wordList[0];
 	wordList.shift();
-	wordList.push(temp);
 }
+
 
 /* request a list of the user's decks from the backend and populate the "select
 your decks" form */
 function getLists() {
-	$.getJSON( 'user_lists.php', function( data ) {
+
+	$('#deck-selection-form').html('');
+
+	var currentURL = URL + 'user_lists.php';
+	$.getJSON( currentURL, function( data ) {
 		if (data.isError === true) {
 			console.log(data.errorTitle);
 			console.log(data.errorDescription);
@@ -176,8 +244,8 @@ function getLists() {
 			});
 		}
 	})
-	 .fail(function() {
-		console.log( "error" );
+	 .fail(function(error) {
+		console.log(error.responseText);
 	});
 }
 
@@ -185,7 +253,8 @@ function getLists() {
 function get_dictionary(word) {
 	$('#translation-panel').show();
 	$('#translation-panel-inner').html('');
-	$.getJSON( 'query_dictionary.php', {query : word }, function( data ) {
+	var currentURL = URL + 'query_dictionary.php';
+	$.getJSON( currentURL, {query : word }, function( data ) {
 		if (data.isError === true) {
 			console.log(data.errorTitle);
 			console.log(data.errorDescription);
@@ -207,6 +276,8 @@ function getCards() {
 	$('#flashcard-pronounce-panel').html('');
 	$('#flashcard-trans-panel').html('');
 
+	$("#success").hide();
+    $("#failure").hide();
 	
 	$('#deck-selection-container').hide();
 	$('#flashcard-container').show();
@@ -232,8 +303,16 @@ function getCards() {
 	$("input[name='wordlist'][type='checkbox']:checked").each(function() {
 		requestedDecks.push(this.id);
 	});
-	var string = JSON.stringify(requestedDecks);
-	$.getJSON( 'user_practice.php', string, function( data ) {
+	var decks = '';
+	for (var i = 0; i < requestedDecks.length - 1; i++) {
+		decks = decks + requestedDecks[i] + ',';
+	}
+	decks = decks + requestedDecks[requestedDecks.length - 1];
+	var currentURL = URL + 'user_practice.php';
+	$.getJSON( currentURL, {
+		list_ids: decks,
+		entries_count: 50
+		}, function( data ) {
 		if (data.isError === true) {
 			console.log(data.errorTitle);
 			console.log(data.errorDescription);
@@ -246,25 +325,40 @@ function getCards() {
 
 /* use the first card to populate the "flashcard view", using view preferences that the user specified earlier */
 function nextCard() {
-	if (showWord) {
-		$('#flashcard-word-panel').html(getWord(wordList[0].word));
+	if (wordList.length === 0) {
+		practice_complete();
 	} else {
-		$('#flashcard-word-panel').html('');
+		if (showWord) {
+			populate_word();
+		} else {
+			$('#flashcard-word-panel').html('');
+		}
+		if (showPronun) {
+			populate_pronoun();
+		} else {
+			$('#flashcard-pronounce-panel').html('');
+		}
+		if (showTrans) {
+			populate_trans();
+		} else {
+			$('#flashcard-trans-panel').html('');
+		}
+		show_full_hide_empty();
 	}
-	if (showPronun) {
-		$('#flashcard-pronounce-panel').html(wordList[0].translation);
-	} else {
-		$('#flashcard-pronounce-panel').html('');
-	}
-	if (showTrans) {
-		$('#flashcard-trans-panel').html(wordList[0].pronunciation);
-	} else {
-		$('#flashcard-trans-panel').html('');
-	}
+}
+
+function practice_complete() {
+	$("#success").html('Practice complete!');
+	$("#success").show();
+	getLists();
+	$('#deck-selection-container').show();
+	$('#flashcard-container').hide();
+	$('#card-followup-container').hide();
 }
 
 /* send student ratings to the backend */
 function send_rating(value) {
-	$.post('entry_results_insert.php', 
+	var currentURL = URL + 'entry_results_insert.php';
+	$.post(currentURL, 
         { 'entry_id' : wordList[0].entryId, 'correctness' : value });
 }
