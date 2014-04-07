1b8bfefb1de964011be0514a86451abe9414dfb9
Yoora Yi Tenen <yooyiten@gmail.com>
2014-04-06 23:02:19 -0400

    Updated navbar for courses

--- frontend/src/js/common.js
+++ frontend/src/js/common.js
@@ -1,4 +1,21 @@
 // To contain global JS functions
+
+function getCourses(){
+	  $.getJSON('../../user_courses.php', 
+        function(data){
+            if(data.isError){
+                // show error
+            }
+            else{
+                $.each(data.result, function(i, item){
+                    courseli = '<li><a href="editcourse.html?course='+item.courseId+'">'+item.name+'</a></li>';
+                    $('#course-menu').append(courseli);
+                });
+            }
+            $('#course-menu').append('<li class="dropdown-header">Other</li><li><a href="#">Search for Courses</a></li>');
+    });
+}
+
 function signout(){
     $.getJSON('../../user_deauthenticate.php', 
         function(data){
