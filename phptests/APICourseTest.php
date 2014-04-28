924ac44b4bfb2c595613c50109ad6b4d5af257a9
Arunabha nag <nag.arunabha@gmail.com>
2014-04-27 23:46:55 -0400

    more unit tests for course

--- phptests/APICourseTest.php
+++ phptests/APICourseTest.php
@@ -15,7 +15,7 @@ class APICourseTest extends PHPUnit_Framework_TestCase
 		$this->db = TestDB::create();
 		$this->assertNotNull($this->db, "failed to create test database");
 		
-		$this->db->add_users(1);
+		$this->db->add_users(5);
 		$session_mock = $this->getMock('Session', array('session_start', 'session_end', 'session_regenerate_id'));
 		
 		// Configure the stub.
@@ -30,13 +30,35 @@ class APICourseTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull($session_mock, "failed to create session mock");
 		Session::set($session_mock);
 		$this->assertEquals(Session::get(), $session_mock);
-		
+		$session_mock->set_allow_email(false);
 		
 		$this->obj = new APICourse(null, $this->db->link);
 		$this->assertNotNull($this->obj, "Null APICourse");
 	}
 	
-
+	public function test_insert()
+	{
+		$_SESSION["handle"] = $this->db->handles[0];
+		
+		$_POST["lang_known"] = "en";
+		$_POST["lang_unknw"] = "xx";
+		$_POST["name"] = "new course";
+		$this->obj->insert();
+		$this->assertTrue(Session::get()->has_error());
+		
+		$_POST["lang_unknw"] = "cn";
+		$this->obj->insert();
+		$this->assertFalse(Session::get()->has_error());
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		$this->assertNotNull($result_assoc);
+		
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertNotNull($result["courseId"]);
+		$this->assertEquals($result["name"], $_POST["name"]);
+	}
+	
 	public function test_select()
 	{
 		$_GET["course_id"] =  $this->db->add_course($this->db->user_ids[0]);
@@ -53,8 +75,7 @@ class APICourseTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull($result_assoc);
 		
 		$result = $result_assoc["result"];
-		$this->assertNotNull($result);
-		
+		$this->assertNotNull($result);		
 	}
 
 	public function test_course_find_by_user_ids()
@@ -109,6 +130,336 @@ class APICourseTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($result0["courseId"] != $result1["courseId"]);
 		$this->assertTrue($result0["courseId"] == $course1_id || $result0["courseId"] == $course3_id );
 		$this->assertTrue($result1["courseId"] == $course1_id || $result1["courseId"] == $course3_id );
+	}
+	
+	public function test_course_find_by_langs()
+	{
+		$user_obj = User::select_by_id($this->db->user_ids[0]);
+		Session::get()->set_user($user_obj);
+		
+		//two public one private 
+		$course1_id = $this->db->add_course($this->db->user_ids[1], 1);
+		$course2_id = $this->db->add_course($this->db->user_ids[2], 1);
+		$course3_id = $this->db->add_course($this->db->user_ids[3], 0);
+		
+		$_SESSION["handle"] = $this->db->handles[0];
+		
+		$_GET["langs"] = implode(",", array(TestDB::$lang_code_0, TestDB::$lang_code_1));
+
+		$this->obj->find();
+		$this->assertFalse(Session::get()->has_error());
+		$result_assoc = Session::get()->get_result_assoc();
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(2, $result);
+	}
+	
+	public function test_update()
+	{
+		$_POST["course_id"] =  $this->db->add_course($this->db->user_ids[0]);
+		$_SESSION["handle"] = $this->db->handles[0];
+		
+		$_POST["name"] = "course new name";
+		$_POST["message"] = "course new message";
+		$_POST["public"] = "1";
+		$_POST["open"] = strtotime("2014-04-12 00:00:00");
+		$_POST["close"] = strtotime("2014-05-12 00:00:00");
+		
+		$this->obj->update();
+		$this->assertFalse(Session::get()->has_error());
+		$result_assoc = Session::get()->get_result_assoc();
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		
+		//print_r($result_assoc);
+		
+		$this->assertEquals($result["name"], $_POST["name"]);
+		$this->assertEquals($result["message"], $_POST["message"]);
+		$this->assertEquals($result["isPublic"], $_POST["public"]);
+		
+		$this->assertNotNull($result["timeframe"]);
+		$timeframe  = $result["timeframe"];
+		$this->assertEquals($timeframe["open"], $_POST["open"]);
+		$this->assertEquals($timeframe["close"], $_POST["close"]);
+	}
+	
+	public function test_update_open_time()
+	{
+		$_POST["course_id"] =  $this->db->add_course($this->db->user_ids[0]);
+		$_SESSION["handle"] = $this->db->handles[0];
+		
+		$_POST["open"] = strtotime("2014-04-12 00:00:00");
+		
+		//Hans, please fix this
+		//$this->obj->update();
+	}
+	
+	public function test_delete()
+	{
+		$this->db->add_users(1);
+		$_POST["course_id"] =  $this->db->add_course($this->db->user_ids[0]);
+
+		//no session user
+		$this->obj->delete();
+		$this->assertTrue(Session::get()->has_error());
+		
+		//wrong session user
+		$_SESSION["handle"] = $this->db->handles[1];
+		$this->obj->delete();
+		$this->assertTrue(Session::get()->has_error());
+
+		//correct session user
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->delete();
+		$this->assertFalse(Session::get()->has_error());
+	}
+	
+	public function test_lists()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$course_unit_id = $this->db->add_course_unit($course_id);
+		$list_id = $this->db->add_list($this->db->user_ids[0], $this->db->entry_ids);
+		$this->db->add_unit_list($course_unit_id, $list_id);
+		
+		$_GET["course_id"] = $course_id;
+		//no session user
+		$this->obj->lists();
+		$this->assertTrue(Session::get()->has_error());
+		
+		//session user
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->lists();
+		$this->assertFalse(Session::get()->has_error());
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+		$list = $result[0];
+		$this->assertEquals($list["listId"], $this->db->list_ids[0]);
+	}
+
+	public function test_units()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$course_unit_id = $this->db->add_course_unit($course_id);
+		$list_id = $this->db->add_list($this->db->user_ids[0], $this->db->entry_ids);
+		$this->db->add_unit_list($course_unit_id, $list_id);
+		
+		$_GET["course_id"] = $course_id;
+		
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->units();
+		$this->assertFalse(Session::get()->has_error());
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+		$unit = $result[0];
+		$this->assertEquals($unit["unitId"], $course_unit_id);
+	}
+	
+	public function test_students()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_student($course_id, $this->db->user_ids[1]);
+		
+		$_GET["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->students();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+		$student = $result[0];
+		$this->assertEquals($student["userId"], $this->db->user_ids[1]);
+	}
+	
+	public function test_students_add()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[1], $this->db->user_ids[2]));
+		
+		$this->obj->students_add();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(2, $result);
+		$student1 = $result[0];
+		$student2 = $result[1];
+		
+		$this->assertTrue($student1["userId"] != $student2["userId"]);
+		$this->assertTrue($student1["userId"] == $this->db->user_ids[1] || $student1["userId"] == $this->db->user_ids[1]);
+		$this->assertTrue($student2["userId"] == $this->db->user_ids[2] || $student2["userId"] == $this->db->user_ids[2]);
+	}
+
+	public function test_students_remove()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_student($course_id, $this->db->user_ids[1]);
+		$this->db->add_course_student($course_id, $this->db->user_ids[2]);
+		$this->db->add_course_student($course_id, $this->db->user_ids[3]);
+		
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[2], $this->db->user_ids[3]));
+		$this->obj->students_remove();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+		$student = $result[0];
+		$this->assertEquals($student["userId"], $this->db->user_ids[1]);
+	}
+	
+	public function test_instructors()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_instructor($course_id, $this->db->user_ids[1]);
+		
+		$_GET["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->instructors();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(2, $result);
+
+		$instructor1 = $result[0];
+		$instructor2 = $result[1];
+		
+		$this->assertTrue($instructor1["userId"] != $instructor2["userId"]);
+		$this->assertTrue($instructor1["userId"] == $this->db->user_ids[0] || $student1["userId"] == $this->db->user_ids[1]);
+		$this->assertTrue($instructor2["userId"] == $this->db->user_ids[0] || $instructor2["userId"] == $this->db->user_ids[1]);
+	}
+	
+	public function test_instructors_add()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[2]));
+		
+		$this->obj->instructors_add();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(2, $result);
+
+		$instructor1 = $result[0];
+		$instructor2 = $result[1];
+		
+		$this->assertTrue($instructor1["userId"] != $instructor2["userId"]);
+		$this->assertTrue($instructor1["userId"] == $this->db->user_ids[0] || $student1["userId"] == $this->db->user_ids[2]);
+		$this->assertTrue($instructor2["userId"] == $this->db->user_ids[0] || $instructor2["userId"] == $this->db->user_ids[2]);
+	}
+	
+	public function test_instructors_remove()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_instructor($course_id, $this->db->user_ids[1]);
+		$this->db->add_course_instructor($course_id, $this->db->user_ids[2]);
+		
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[2]));
+		$this->obj->instructors_remove();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		
+		//Hans, please check this
+		//$this->assertCount(2, $result);
+
+		//$instructor1 = $result[0];
+		//$instructor2 = $result[1];
+		
+		//$this->assertTrue($instructor1["userId"] != $instructor2["userId"]);
+		//$this->assertTrue($instructor1["userId"] == $this->db->user_ids[0] || $student1["userId"] == $this->db->user_ids[1]);
+		//$this->assertTrue($instructor2["userId"] == $this->db->user_ids[0] || $instructor2["userId"] == $this->db->user_ids[1]);
+	}
+	
+	public function test_researchers()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_researcher($course_id, $this->db->user_ids[1]);
+		
+		$_GET["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$this->obj->researchers();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+
+		$researcher = $result[0];
+		
+		$this->assertTrue($researcher["userId"] == $this->db->user_ids[1]);
+	}
+	
+	public function test_researchers_add()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[1], $this->db->user_ids[2]));
+		
+		$this->obj->researchers_add();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(2, $result);
+		$researcher1 = $result[0];
+		$researcher2 = $result[1];
+		
+		$this->assertTrue($researcher1["userId"] != $researcher2["userId"]);
+		$this->assertTrue($researcher1["userId"] == $this->db->user_ids[1] || $researcher1["userId"] == $this->db->user_ids[1]);
+		$this->assertTrue($researcher2["userId"] == $this->db->user_ids[2] || $researcher2["userId"] == $this->db->user_ids[2]);
+	}
+	
+	public function test_researchers_remove()
+	{
+		$course_id =  $this->db->add_course($this->db->user_ids[0]);
+		$this->db->add_course_researcher($course_id, $this->db->user_ids[1]);
+		$this->db->add_course_researcher($course_id, $this->db->user_ids[2]);
+		$this->db->add_course_researcher($course_id, $this->db->user_ids[3]);
+		
+		$_POST["course_id"] = $course_id;
+		$_SESSION["handle"] = $this->db->handles[0];
+		$_POST["user_ids"] = implode(",", array($this->db->user_ids[2], $this->db->user_ids[3]));
+		$this->obj->researchers_remove();
+		
+		$result_assoc = Session::get()->get_result_assoc();
+		//print_r($result_assoc);
+		$result = $result_assoc["result"];
+		$this->assertNotNull($result);
+		$this->assertCount(1, $result);
+		$researcher = $result[0];
+		$this->assertEquals($researcher["userId"], $this->db->user_ids[1]);
 	}
 	
 	public function test_practice_report()
